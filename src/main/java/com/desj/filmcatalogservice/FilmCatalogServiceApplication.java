3eb8fd1dad268e92f1419e5d6bd7b460c356ed79
Gabriel <gabrielstrzal@gmail.com>
2022-07-04 21:57:29 -0400

    initial Setup

---
+++ src/main/java/com/desj/filmcatalogservice/FilmCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.desj.filmcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desj/filmcatalogservice/FilmCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desj.filmcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
